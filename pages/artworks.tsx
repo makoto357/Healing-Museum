3a271a6f8bcca16d2aa7eb8ec3414ee2c151b575
clairechu <makoto357@gmail.com>
2022-10-31 18:08:10 +0800

    fetch museum api

--- pages/artworks.tsx
+++ pages/artworks.tsx
@@ -1,6 +1,34 @@
 import Link from "next/link";
+import { useEffect, useRef, useState, useContext } from "react";
 
 export default function Artworks() {
+  const [artworkID, setArtworkID] = useState([]);
+  useEffect(() => {
+    const getArtist = async () => {
+      const res = await fetch(
+        "https://collectionapi.metmuseum.org/public/collection/v1/search?q=sunflowers"
+      );
+      const result = await res.json();
+      console.log(result.objectIDs);
+      setArtworkID(result.objectIDs);
+    };
+    getArtist();
+  }, []);
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     fetch(
+  //       "https://collectionapi.metmuseum.org/public/collection/v1/search?q=sunflowers"
+  //     )
+  //       .then((res) => res.json())
+  //       .then((result) => {
+  //         console.log(result);
+  //       })
+  //       .catch(console.log);
+  //   };
+  //   fetchData();
+  // }, []);
+
   return (
     <>
       <Link href="/">
