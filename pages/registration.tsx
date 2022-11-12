30a171acf70b63a1e42c63eadfbf7a818ba463a4
clairechu <makoto357@gmail.com>
2022-11-12 20:32:56 +0800

    layout sign-in page

--- pages/registration.tsx
+++ pages/registration.tsx
@@ -8,6 +8,96 @@ import { useAuth } from "../context/AuthContext";
 import { doc, setDoc, Timestamp } from "firebase/firestore";
 import { db } from "../config/firebase";
 
+const Wrapper = styled.div`
+  margin: auto;
+  padding: 40px 56px 40px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: left;
+`;
+
+const Heading = styled.h1`
+  width: 100%;
+  max-width: 458px;
+  margin-bottom: 30px;
+  font-size: 20px;
+`;
+
+const Form = styled.form`
+  width: 100%;
+  max-width: 458px;
+`;
+
+const FormControl = styled.input`
+  font-size: 15px;
+  padding: 15px;
+  width: 100%;
+  margin-bottom: 15px;
+  border-radius: 5px;
+`;
+// border: solid 1px ${({ invalid }) => (invalid ? "#CB4042" : "#979797")};
+
+const FormSplit = styled.div`
+  display: flex;
+  margin-bottom: 10px;
+  width: 100%;
+  max-width: 458px;
+`;
+
+const Split = styled.div`
+  width: 45%;
+  border-bottom: 1px solid #313538;
+  margin-bottom: 15px;
+`;
+
+const SplitText = styled.div`
+  margin-top: 10px 10px 0px;
+  padding: 10px;
+`;
+
+const Signup = styled.div`
+  display: flex;
+  width: 100%;
+  max-width: 458px;
+  justify-content: flex-end;
+`;
+
+const MemberSignup = styled.div`
+  border-bottom: 1px solid;
+  cursor: pointer;
+`;
+
+const Button = styled.button`
+  font-size: 15px;
+  padding: 15px;
+  width: 100%;
+  margin-bottom: 10px;
+  color: white;
+  background-color: #2c2b2c;
+  border: 1px solid #2c2b2c;
+  cursor: pointer;
+  border-radius: 30px;
+`;
+
+const FBbutton = styled.button`
+  font-size: 15px;
+  padding: 15px;
+  width: 100%;
+  margin-bottom: 15px;
+  color: #3b5998;
+  border: 1px solid #3b5998;
+  cursor: pointer;
+  background-color: white;
+  max-width: 458px;
+  border-radius: 30px;
+  &:hover {
+    transition: all 0.3s ease;
+    background-color: #3b5998;
+    color: white;
+  }
+`;
+
 export default function LoginPage() {
   const { user, login, signup } = useAuth();
   const [isSignedUp, setIsSignedUp] = useState(true);
@@ -64,159 +154,131 @@ export default function LoginPage() {
   };
 
   return (
-    <>
+    <Wrapper>
       {isSignedUp ? (
-        <section style={{ marginBottom: "5px" }}>
-          <div>
+        <>
+          <Heading>
+            Welcome back! <br />
+            Please log in to visit:
+          </Heading>
+          <Form action="#" onSubmit={handleLogin}>
             <div>
-              <div>
-                <h1>
-                  Welcome back! <br />
-                  Please log in to visit:
-                </h1>
-                <form action="#" onSubmit={handleLogin}>
-                  <div>
-                    <label htmlFor="email">Your email</label>
-                    <input
-                      type="email"
-                      name="email"
-                      id="email"
-                      placeholder="name@company.com"
-                      required
-                      onChange={(e) =>
-                        setLoginData({
-                          ...loginData,
-                          email: e.target.value,
-                        })
-                      }
-                      value={loginData.email}
-                    />
-                  </div>
-                  <div>
-                    <label htmlFor="password">Password</label>
-                    <input
-                      type="password"
-                      name="password"
-                      id="password"
-                      placeholder="••••••••"
-                      required
-                      onChange={(e) =>
-                        setLoginData({
-                          ...loginData,
-                          password: e.target.value,
-                        })
-                      }
-                      value={loginData.password}
-                    />
-                  </div>
+              <label htmlFor="email">Email</label>
+              <FormControl
+                type="email"
+                name="email"
+                id="email"
+                placeholder="name@company.com"
+                required
+                onChange={(e) =>
+                  setLoginData({
+                    ...loginData,
+                    email: e.target.value,
+                  })
+                }
+                value={loginData.email}
+              />
+            </div>
+            <div>
+              <label htmlFor="password">Password</label>
+              <FormControl
+                type="password"
+                name="password"
+                id="password"
+                placeholder="••••••••"
+                required
+                onChange={(e) =>
+                  setLoginData({
+                    ...loginData,
+                    password: e.target.value,
+                  })
+                }
+                value={loginData.password}
+              />
+            </div>
 
-                  <button
-                    type="submit"
-                    style={{ border: "1px solid black", padding: "5px 10px" }}
-                  >
-                    Log in
-                  </button>
-                </form>
-                <div style={{ display: "flex" }}>
-                  <p>
-                    First time here? Please{" "}
-                    <button onClick={() => setIsSignedUp(false)}>
-                      Sign up
-                    </button>{" "}
-                    this way
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
-      ) : (
-        <section>
+            <Button type="submit">Log in</Button>
+          </Form>
           <div>
-            <div>
-              <div>
-                <h1>
-                  First Time Here?
-                  <br />
-                  Sign up to Share and Collect Artworks!
-                </h1>
-                <form action="#" onSubmit={handleSignup}>
-                  <div>
-                    <label htmlFor="email">Your name</label>
-                    <input
-                      type="text"
-                      name="username"
-                      id="username"
-                      placeholder="user name"
-                      required
-                      onChange={(e: any) =>
-                        setSignupData({
-                          ...signupData,
-                          username: e.target.value,
-                        })
-                      }
-                      value={signupData.username}
-                    />
-                  </div>
-                  <div>
-                    <label htmlFor="email">Your email</label>
-                    <input
-                      type="email"
-                      name="email"
-                      id="email"
-                      placeholder="name@company.com"
-                      required
-                      onChange={(e: any) =>
-                        setSignupData({
-                          ...signupData,
-                          email: e.target.value,
-                        })
-                      }
-                      value={signupData.email}
-                    />
-                  </div>
-                  <div>
-                    <label htmlFor="password">Password</label>
-                    <input
-                      type="password"
-                      name="password"
-                      id="password"
-                      placeholder="••••••••"
-                      required
-                      onChange={(e: any) =>
-                        setSignupData({
-                          ...signupData,
-                          password: e.target.value,
-                        })
-                      }
-                      value={signupData.password}
-                    />
-                  </div>
-                  <button
-                    type="submit"
-                    style={{ border: "1px solid black", padding: "5px 10px" }}
-                  >
-                    Create an account
-                  </button>
-                </form>
-                <div style={{ display: "flex" }}>
-                  <p>
-                    Already been here before? Simply
-                    <button onClick={() => setIsSignedUp(true)}>
-                      Log in
-                    </button>{" "}
-                  </p>
-                </div>
-              </div>
-            </div>
+            <p>
+              First time here? Please{" "}
+              <button onClick={() => setIsSignedUp(false)}>Sign up</button> this
+              way.
+            </p>
           </div>
-        </section>
+        </>
+      ) : (
+        <>
+          <Heading>
+            Sign up to <br />
+            collect and share artworks!
+          </Heading>
+          <Form action="#" onSubmit={handleSignup}>
+            <div>
+              <label htmlFor="email">Name</label>
+              <FormControl
+                type="text"
+                name="username"
+                id="username"
+                placeholder="Please enter your name"
+                required
+                onChange={(e: any) =>
+                  setSignupData({
+                    ...signupData,
+                    username: e.target.value,
+                  })
+                }
+                value={signupData.username}
+              />
+            </div>
+            <div>
+              <label htmlFor="email">Email</label>
+              <FormControl
+                type="email"
+                name="email"
+                id="email"
+                placeholder="name@mail.com"
+                required
+                onChange={(e: any) =>
+                  setSignupData({
+                    ...signupData,
+                    email: e.target.value,
+                  })
+                }
+                value={signupData.email}
+              />
+            </div>
+            <div>
+              <label htmlFor="password">Password</label>
+              <FormControl
+                type="password"
+                name="password"
+                id="password"
+                placeholder="••••••••"
+                required
+                onChange={(e: any) =>
+                  setSignupData({
+                    ...signupData,
+                    password: e.target.value,
+                  })
+                }
+                value={signupData.password}
+              />
+            </div>
+            <Button type="submit">Create an account</Button>
+          </Form>
+          <div>
+            <p>
+              Already been here before? Simply{" "}
+              <button onClick={() => setIsSignedUp(true)}>Log in</button>.
+            </p>
+          </div>
+        </>
       )}
       <h1>
         <strong>OR</strong>
       </h1>
-
-      <Link href="/theme-color">Directly Enter the Museum </Link>
-    </>
+      <Link href="/theme-color">Directly enter the museum!</Link>
+    </Wrapper>
   );
 }
