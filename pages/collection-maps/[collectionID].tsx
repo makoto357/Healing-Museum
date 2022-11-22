8dfdf4a42f4cc77b2393ca2f8b9a327d2607eca1
clairechu <makoto357@gmail.com>
2022-11-22 11:02:51 +0800

    layout texts on collectionID page

--- pages/collection-maps/[collectionID].tsx
+++ pages/collection-maps/[collectionID].tsx
@@ -4,18 +4,15 @@ import dynamic from "next/dynamic";
 import { useRouter } from "next/router";
 import {
   collection,
-  onSnapshot,
   doc,
   updateDoc,
   arrayUnion,
-  arrayRemove,
   query,
   where,
   getDocs,
-  orderBy,
 } from "firebase/firestore";
 import { db } from "../../config/firebase";
-import { useRef, useState, useEffect, useContext } from "react";
+import { useState, useEffect, useContext } from "react";
 import { useAuth } from "../../context/AuthContext";
 import { ThemeColorContext } from "../../context/ColorContext";
 import ZoomModal from "../../components/ZoomModal";
@@ -45,8 +42,7 @@ const SizeController = styled.div`
 `;
 const TextWrapper = styled.section`
   margin: 24px auto 0;
-  width: fit-content;
-  max-width: 80vw;
+  width: 60vw;
 `;
 
 const TextHeader = styled.section`
@@ -57,7 +53,7 @@ const TextHeader = styled.section`
 const IconGroup = styled.div`
   display: flex;
   column-gap: 20px;
-  margin-left: 30px;
+  margin: 10px 0 0 30px;
 `;
 
 const LikeButton = styled.div`
@@ -73,56 +69,63 @@ const DescriptionList = styled.div`
 `;
 
 const CloseIcon = styled.button`
-  background: #262626;
+  background: white;
   border-radius: 50px;
   border: none;
   cursor: pointer;
   margin-left: 20px;
   opacity: 0.8;
   padding: 0;
-  width: 35px;
-  height: 35px;
+  width: 50px;
+  height: 50px;
   position: absolute;
-  top: 1rem;
+  top: 0;
   right: 2rem;
   z-index: 200;
+  padding-left: 6px;
   &:hover {
-    transition: border 0.3s;
+    transition: border 0.5s;
     border: 1px solid white;
-    // transform: translateY(calc(24px + 0.75vw)) scale(1);
+    height: 48px;
+    width: 48px;
   }
 `;
 
 const ZoomIcon = styled.button`
-  background: #262626;
+  padding-top: 7px;
+  padding-left: 6px;
+  background: white;
   border-radius: 50px;
   border: none;
   cursor: pointer;
   opacity: 0.8;
-  width: 35px;
-  height: 35px;
+  width: 50px;
+  height: 50px;
   position: absolute;
   left: 2rem;
   z-index: 500;
   &:hover {
-    transition: border 0.3s;
+    transition: border 0.5s;
     border: 1px solid white;
-    // transform: translateY(calc(24px + 0.75vw)) scale(1);
+    height: 48px;
+    width: 48px;
   }
 `;
 
 const MagnifyingGlassWrapper = styled.div`
   position: absolute;
   bottom: -20px;
-  right: 40px;
+  right: 20px;
   border-radius: 50%;
   cursor: pointer;
   height: 40px;
   width: 40px;
-  background: white;
+  background: #e1ddd6;
   &:hover {
     transition: border 0.5s;
-    border: 1px solid black;
+    border: 1px solid #e1ddd6;
+    height: 38px;
+    width: 38px;
   }
 `;
 
@@ -134,7 +137,7 @@ const MagnifyingGlass = styled.div`
   z-index: 200;
   border: none;
   cursor: pointer;
-  background: white;
+  background: #e1ddd6;
 `;
 export default function ArtworkDetail() {
   const { user } = useAuth();
@@ -229,7 +232,7 @@ export default function ArtworkDetail() {
   const [themeColor] = useContext(ThemeColorContext);
   console.log(themeColor);
   return (
-    <>
+    <div style={{ paddingTop: "104px" }}>
       {artwork &&
         artwork?.map((artwork, index) => (
           <ArtworkWrapper key={index}>
@@ -248,11 +251,11 @@ export default function ArtworkDetail() {
             <TextWrapper>
               <TextHeader>
                 <div>
-                  <h1>
+                  <h1 style={{ fontSize: "1.75rem" }}>
                     <strong>{artwork.title}</strong>
                     <strong></strong>
                   </h1>
-                  <h2>
+                  <h2 style={{ fontSize: "1.5rem" }}>
                     <span>{artwork.artistName}, </span>
                     <span>{artwork.completitionYear}</span>
                   </h2>
@@ -293,16 +296,24 @@ export default function ArtworkDetail() {
                   <strong>Collection: </strong>
                   {artwork.galleries}
                 </li>
-                <li>
-                  {artwork?.tags?.map((tag, index) => (
-                    <span key={index}>
-                      {" "}
-                      <strong>#</strong>
-                      {tag}{" "}
-                    </span>
-                  ))}
-                </li>
               </DescriptionList>
+              <li style={{ listStyle: "none" }}>
+                {artwork?.tags?.map((tag, index) => (
+                  <span
+                    style={{
+                      background: "#e1ddd6",
+                      marginRight: "10px",
+                      borderRadius: "5px",
+                      padding: "5px",
+                    }}
+                    key={index}
+                  >
+                    {" "}
+                    <strong>#</strong>
+                    {tag}{" "}
+                  </span>
+                ))}
+              </li>
             </TextWrapper>
           </ArtworkWrapper>
         ))}
@@ -331,12 +342,12 @@ export default function ArtworkDetail() {
                           <path
                             d="M17.5 8.086v18.875M8.088 17.5h18.813"
                             fill="none"
-                            stroke="#fff"
+                            stroke="black"
                           ></path>
                         </svg>
                       </ZoomIcon>
                       <ZoomIcon
-                        style={{ top: "3rem" }}
+                        style={{ top: "4rem" }}
                         onClick={() => zoomOut()}
                         aria-label="Zoom out"
                       >
@@ -346,7 +357,7 @@ export default function ArtworkDetail() {
                         >
                           <path
                             fill="none"
-                            stroke="#fff"
+                            stroke="black"
                             d="M8.088 17.5h18.813"
                           ></path>
                         </svg>
@@ -375,7 +386,7 @@ export default function ArtworkDetail() {
                         <path
                           d="M24.251 10.935L10.746 24.12m.194-13.344l13.143 13.462"
                           fill="none"
-                          stroke="#fff"
+                          stroke="black"
                         ></path>
                       </svg>
                     </CloseIcon>
@@ -391,11 +402,11 @@ export default function ArtworkDetail() {
         </Link>
       </div>
 
-      <div style={{ textAlign: "right" }}>
+      <div style={{ textAlign: "left" }}>
         <Link href="/artworks">
           <p>Explore more artworks!</p>
         </Link>
       </div>
-    </>
+    </div>
   );
 }
