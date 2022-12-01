5a9f4f41347a6fdb204189b124e300b990ec8910
clairechu <makoto357@gmail.com>
2022-12-01 17:19:08 +0800

    add feature column to homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,19 +7,7 @@ import { useAuth } from "../context/AuthContext";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { useTranslation } from "next-i18next";
 import { FacebookShareButton } from "next-share";
-import github from "../asset/github.png";
-import fb from "../asset/fb.svg";
-import profile from "../asset/profile.png";
-import logout from "../asset/log-out.png";
-
-import brandIcon from "../asset/healing-museum-website-icon.png";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import close from "../asset/cancel-white.png";
-import toggle from "../asset/menu.png";
-import language from "../asset/translate.png";
 import { useRouter } from "next/router";
-import SignpostButton from "../components/Button";
 import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
@@ -27,9 +15,52 @@ import "swiper/css/navigation";
 import "swiper/css/effect-fade";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
-// import museumOfMind from "../asset/museum-of-the-mind.jpeg";
+import museumOfMind from "../asset/museum-of-the-mind.jpeg";
 import happiness from "../asset/happiness.png";
 import benUri from "../asset/ben-uri.jpeg";
+import colors from "../asset/pantone.png";
+import quiz from "../asset/ideas.png";
+import map from "../asset/journal.png";
+import post from "../asset/video-player.png";
+import ticket from "../asset/ticket.png";
+import arrow from "../asset/right-arrow.png";
+import github from "../asset/github.png";
+import fb from "../asset/fb.svg";
+import share from "../asset/share.png";
+import close from "../asset/cancel-white.png";
+import toggle from "../asset/menu.png";
+import language from "../asset/translate.png";
+
+const TranslationToggle = styled.div`
+  background-image: url(${language.src});
+  background-size: cover;
+  height: 30px;
+  width: 30px;
+  position: absolute;
+  top: 24px;
+  left: 64px;
+  cursor: pointer;
+`;
+
+const MenuToggle = styled.div`
+  background-image: url(${toggle.src});
+  background-size: cover;
+  height: 30px;
+  width: 30px;
+  position: absolute;
+  top: 24px;
+  left: 24px;
+  cursor: pointer;
+`;
+
+const CloseMenuIcon = styled.div`
+  background-image: url(${close.src});
+  width: 20px;
+  height: 20px;
+  background-size: cover;
+  margin-left: auto;
+`;
+
 const Menulist = styled.ul<{ $menuStyle: string }>`
   z-index: 20;
   position: absolute;
@@ -47,36 +78,6 @@ const Menulist = styled.ul<{ $menuStyle: string }>`
   justify-content: space-between;
 `;
 
-const CloseMenuIcon = styled.div`
-  background-image: url(${close.src});
-  width: 20px;
-  height: 20px;
-  background-size: cover;
-  margin-left: auto;
-`;
-
-const MenuToggle = styled.div`
-  background-image: url(${toggle.src});
-  background-size: cover;
-  height: 30px;
-  width: 30px;
-  position: fixed;
-  top: 24px;
-  left: 24px;
-  cursor: pointer;
-`;
-
-const TranslationToggle = styled.div`
-  background-image: url(${language.src});
-  background-size: cover;
-  height: 30px;
-  width: 30px;
-  position: fixed;
-  top: 24px;
-  left: 64px;
-  cursor: pointer;
-`;
-
 const Page = styled.div`
   background-image: linear-gradient(180deg, transparent 95%, white 0);
   background-repeat: no-repeat;
@@ -89,38 +90,64 @@ const Page = styled.div`
   }
 `;
 const FBicon = styled.div`
-  background-image: url(${fb.src});
+  background-image: url(${share.src});
+  border: 2px solid #2c2b2c;
   width: 30px;
   height: 30px;
   background-size: cover;
   &:hover {
-    border: 2px solid #2c2b2c;
-  }
-`;
-const ProfileIcon = styled.div`
-  background-image: url(${profile.src});
-  width: 30px;
-  height: 30px;
-  background-size: cover;
-  &:hover {
-    border-radius: 50%;
-    width: 30px;
-    height: 30px;
-    border: 2px solid #2c2b2c;
+    width: 32px;
+    height: 32px;
   }
 `;
 
-const LogoutIcon = styled.div`
-  background-image: url(${logout.src});
-  width: 30px;
-  height: 30px;
+const MainVisual = styled.div`
+  background-image: url(${museumOfMind.src});
+  opacity: 0.8;
   background-size: cover;
-  &:hover {
-    border-radius: 50%;
-    width: 30px;
-    height: 30px;
-    border: 2px solid #2c2b2c;
+  position: relative;
+  height: 450px;
+  width: 100vw;
+  background-position: center;
+`;
+const EntranceIcon = styled.div`
+  width: 45px;
+  height: 45px;
+  background-size: cover;
+  background-image: url(${ticket.src});
+`;
+
+const ArrowIcon = styled.div`
+  width: 12px;
+  height: 12px;
+  background-size: cover;
+  background-image: url(${arrow.src});
+`;
+
+const IntroText = styled.div`
+  display: flex;
+  column-gap: 30px;
+  width: 80vw;
+  margin-bottom: 20px;
+  @media screen and (max-width: 600px) {
+    display: initial;
   }
+`;
+
+const FeatureIconGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 150px;
+  text-align: left;
+  justify-content: center;
+  padding: 0 5px;
+`;
+
+const FeatureIcon = styled.div`
+  width: 45px;
+  height: 45px;
+  background-size: cover;
+  margin: 0 auto 20px;
 `;
 
 const SwiperWrapper = styled.section`
@@ -129,21 +156,15 @@ const SwiperWrapper = styled.section`
 `;
 
 const bannerImages = [
-  // {
-  //   image: museumOfMind.src,
-  //   caption: "",
-  // "The Bethlem Museum of the Mind in London, situated within the gounds of Bethlem Royal Hospital, holds an internationally renowned collection of archives, art and historic objects, which together offer an unparalleled resource to support the history of mental healthcare and treatment. ",
-  //   webpage: "https://museumofthemind.org.uk/",
-  // },
   {
     image: happiness.src,
-    caption: "Exhibiitons on wellbeing at the Wellcome Collection.",
+    caption: "index:caption1",
     webpage:
       "https://wellcomecollection.org/?gclid=Cj0KCQiAveebBhD_ARIsAFaAvrHyq9spdBGfJbkRv20ZALgIwL1m7W0kWCU0daQiTQ-wG9UMLKhnfwQaAoc1EALw_wcB",
   },
   {
     image: benUri.src,
-    caption: "Art therapy session in the Ben Uri Museum and Gallery.",
+    caption: "index:caption2",
     webpage: "https://benuri.org/artsandhealth/",
   },
 ];
@@ -158,6 +179,22 @@ const menuLinks = [
   { link: "/form", text: "index:form" },
   { link: "/visitor-posts", text: "index:posts" },
 ];
+
+const websiteFeatures = [
+  {
+    icon: colors.src,
+    text: "index:selectColor",
+  },
+  { icon: quiz.src, text: "index:artQuiz" },
+  {
+    icon: map.src,
+    text: "index:audiovisual",
+  },
+  {
+    icon: post.src,
+    text: "index:postFeeling",
+  },
+];
 export async function getStaticProps({ locale }) {
   return {
     props: {
@@ -167,30 +204,12 @@ export async function getStaticProps({ locale }) {
 }
 export default function Home(props) {
   const { t } = useTranslation();
-  const { user, logout } = useAuth();
+  const { user } = useAuth();
   const router = useRouter();
   const { locale } = router;
   const [showMenu, setShowMenu] = useState(false);
   return (
     <>
-      <ToastContainer
-        position="top-center"
-        // autoClose={3000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="light"
-      />
-      <MenuToggle
-        role="button"
-        onClick={() => {
-          setShowMenu(true);
-        }}
-      ></MenuToggle>
       {locale == "en" ? (
         <Link href="/" locale="zh">
           <TranslationToggle role="button"></TranslationToggle>
@@ -200,6 +219,13 @@ export default function Home(props) {
           <TranslationToggle role="button"></TranslationToggle>
         </Link>
       )}
+
+      <MenuToggle
+        role="button"
+        onClick={() => {
+          setShowMenu(true);
+        }}
+      ></MenuToggle>
       <Menulist $menuStyle={!showMenu ? "translateX(-100%)" : "none"}>
         <div>
           <CloseMenuIcon
@@ -237,98 +263,123 @@ export default function Home(props) {
           >
             <FBicon />
           </FacebookShareButton>
-
-          {/* {user && (
-            <li>
-              <Link href="/user-profile">
-                <ProfileIcon />{" "}
-              </Link>
-            </li>
-          )} */}
-          {/* <li>
-                <div
-                  onClick={() => {
-                    logout();
-                  }}
-                >
-                  <LogoutIcon />
-                </div>
-              </li> */}
         </div>
       </Menulist>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          margin: "0 70px",
-          padding: "40px 0 0",
-          alignItems: "center",
-        }}
-      >
+      <div style={{ display: "block", paddingTop: "14px" }}>
+        <MainVisual title={t("index:museumOfMind")} />
+
+        <div
+          style={{
+            borderTop: "1px solid black",
+            borderBottom: "1px solid black",
+            display: "flex",
+            justifyContent: "center",
+            paddingRight: "20px",
+            alignItems: "center",
+            height: "60px",
+          }}
+        >
+          <EntranceIcon />
+
+          <Link
+            style={{
+              cursor: "pointer",
+              margin: "auto 3px auto 10px",
+              fontSize: "1.25rem",
+              fontWeight: "600",
+            }}
+            href="/registration"
+          >
+            {t("index:start")}
+          </Link>
+          <ArrowIcon />
+        </div>
+        <h1
+          style={{
+            fontSize: "1.25rem",
+            textAlign: "center",
+            margin: "40px auto 20px",
+          }}
+        >
+          <strong> {t("index:experience")}</strong>
+        </h1>
+        <section
+          style={{
+            display: "flex",
+            height: "fit-content",
+            width: "80vw",
+            margin: "20px auto",
+            justifyContent: "space-between",
+            alignItems: "baseline",
+          }}
+        >
+          {websiteFeatures.map((websiteFeature) => (
+            <FeatureIconGroup key={websiteFeature.icon}>
+              <FeatureIcon
+                style={{ backgroundImage: `url(${websiteFeature.icon})` }}
+              />
+              <p>{t(websiteFeature.text)}</p>
+            </FeatureIconGroup>
+          ))}
+        </section>
+
         <div
           style={{
             display: "flex",
-            columnGap: "30px",
-            width: "80vw",
+            flexDirection: "column",
+            margin: "0 70px",
+            padding: "40px 0 0",
+            alignItems: "center",
           }}
         >
-          <p>
-            The Healing Museum is an online experience inspired by two concepts:
-            “art as therapy” and “museum on prescription”. The former has its
-            root in the 19th century psychologists’ theories and their
-            perspective of “artistic expression as a window into the inner
-            workings of the mind”, while the latter considers museums as places
-            with potential to facilitate the healing process as well as serving
-            as social prescriptions to complement primary medical care.
-          </p>
-          <p>
-            Art as healing means to direct personal or traumatic experiences
-            towards creative expressions of who you are, how you feel and what
-            you identify with. By presenting the artworks of 3 female and 3 male
-            modern artists, who have shared their visions of realities and
-            transcended their adversities into ever-lasting arts, the Healing
-            Museum aims to explore the source of their strength and learn about
-            resilience.
-          </p>
-          {/* <br />
-            We invite you to enter this Museum, and enjoy the colors and stories
-            of these artists through a series of interactive experiences.
-            Starting from choosing a color which echoes with your mood, and a
-            quiz of which the questions are based on anecdotes of the artists,
-            you will be going on a journey to the inner worlds and great minds
-            of the artists, accompanied by audio-visual materials. At the end of
-            your visits, we hope to bring you a bit of feelings and
-            inspirations. */}
-        </div>
-        <SignpostButton href="/registration">Start your journey</SignpostButton>
-
-        <SwiperWrapper>
-          <Swiper
-            modules={[Navigation, Pagination, Scrollbar, A11y]}
-            spaceBetween={50}
-            slidesPerView={1}
-            navigation
-            loop
-            onSwiper={(swiper) => console.log(swiper)}
-            onSlideChange={() => console.log("slide change")}
+          <h1
+            style={{
+              fontSize: "1.25rem",
+              marginBottom: "20px",
+            }}
           >
-            {bannerImages.map((bannerImage) => (
-              <SwiperSlide key={bannerImage.image}>
-                <div>
-                  <Image
-                    src={bannerImage.image}
-                    width={1280}
-                    height={720}
-                    alt={bannerImage.caption}
-                  />
-                  <h1 style={{ fontSize: "0.5rem" }}>
-                    <strong>{bannerImage.caption}</strong>
-                  </h1>
-                </div>
-              </SwiperSlide>
-            ))}
-          </Swiper>
-        </SwiperWrapper>
+            <strong> {t("index:museum")}</strong>
+          </h1>
+          <IntroText>
+            <p>{t("index:paragraph1")}</p>
+            <br />
+            <p>{t("index:paragraph2")}</p>
+          </IntroText>
+
+          <SwiperWrapper>
+            <Swiper
+              modules={[Navigation, Pagination, Scrollbar, A11y]}
+              spaceBetween={50}
+              slidesPerView={1}
+              navigation
+              loop
+              onSwiper={(swiper) => console.log(swiper)}
+              onSlideChange={() => console.log("slide change")}
+            >
+              {bannerImages.map((bannerImage) => (
+                <SwiperSlide
+                  key={bannerImage.image}
+                  style={{ cursor: "pointer" }}
+                >
+                  <div>
+                    <Link href={bannerImage.webpage}>
+                      <Image
+                        style={{ cursor: "pointer" }}
+                        src={bannerImage.image}
+                        width={1280}
+                        height={720}
+                        alt={bannerImage.caption}
+                      />
+                    </Link>
+                    <h1 style={{ fontSize: "0.75rem" }}>
+                      <strong>{t(bannerImage.caption)}</strong>
+                    </h1>
+                  </div>
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </SwiperWrapper>
+        </div>
       </div>
     </>
   );
