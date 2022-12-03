f5ceb8947007b3ee5c74adcfdc29005c9fa830b5
clairechu <makoto357@gmail.com>
2022-12-03 15:55:25 +0800

    user profile page: add drag and drop feature

---
+++ components/DragNDrop.tsx
@@ -0,0 +1,234 @@
+import styled from "@emotion/styled";
+import { Droppable, Draggable } from "react-beautiful-dnd";
+import drag from "../asset/drag.png";
+const CollectionWrapper = styled.div`
+  display: flex;
+  @media screen and (max-width: 650px) {
+    flex-direction: column;
+  }
+`;
+const TextScreen = styled.div<{ $displayText: string }>`
+  background: black;
+  height: 100%;
+  position: absolute;
+  padding: 10px;
+  text-align: left;
+  opacity: 0.5;
+  color: white;
+  width: 100%;
+  display: ${(props) => props.$displayText};
+  z-index: 10;
+  overflow-y: scroll;
+  white-space: normal;
+`;
+const PostWrapper = styled.div`
+  padding: 5px 10px;
+  height: 150px;
+  width: 150px;
+  border: 1px solid black;
+  display: flex;
+  flex-direction: column;
+  row-gap: 15px;
+  overflow-y: scroll;
+  white-space: normal;
+
+  &:hover {
+    background-color: #2c2b2c;
+    color: white;
+    opacity: 0.5;
+  }
+`;
+const DragInstruction = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: auto 2vw;
+  @media screen and (max-width: 650px) {
+    flex-direction: row;
+    margin: 20px auto;
+  }
+`;
+const DragButton = styled.div`
+  background-image: url(${drag.src});
+  margin: 0 auto;
+  width: 50px;
+  height: 50px;
+  background-size: cover;
+  @media screen and (max-width: 650px) {
+    margin-right: 10px;
+  }
+`;
+const getListStyle = (isDraggingOver) => ({
+  whiteSpace: "nowrap",
+  overflow: "scroll",
+});
+
+const getItemStyle = (isDragging, draggableStyle) => ({
+  userSelect: "none",
+  width: 150,
+  height: 150,
+  display: "inline-block",
+  overflow: "scroll",
+  margin: `0 20px 0 0`,
+
+  ...draggableStyle,
+});
+
+export default function CollectionColumn({
+  showFavoriteArtworks,
+  artwork,
+  setShowText,
+  showText,
+  favoritePosts,
+  dragNDrop,
+  setDragNDrop,
+}) {
+  console.log(favoritePosts, artwork);
+  return (
+    <CollectionWrapper>
+      <div
+        style={{
+          border: "1px solid black",
+          height: "180px",
+
+          padding: "13px",
+          width: "80vw",
+          marginLeft: "5vw",
+        }}
+      >
+        <Droppable direction="horizontal" droppableId="drop-id">
+          {(droppableProvided, droppableSnapshot) => (
+            <div
+              {...droppableProvided.droppableProps}
+              ref={droppableProvided.innerRef}
+              style={getListStyle(droppableSnapshot.isDraggingOver)}
+              //flex here
+            >
+              {showFavoriteArtworks &&
+                artwork.map((artwork, index) => {
+                  // const Template = templatesArr[page.type];
+                  return (
+                    <Draggable
+                      draggableId={artwork.id}
+                      index={index}
+                      key={artwork.id}
+                    >
+                      {(
+                        provided,
+                        snapshot ///change var names
+                      ) => (
+                        <div
+                          ref={provided.innerRef}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                          style={getItemStyle(
+                            snapshot.isDragging,
+                            provided.draggableProps.style
+                          )}
+                        >
+                          {/* independent div */}
+                          <div
+                            onMouseEnter={() => setShowText(artwork.id)}
+                            onMouseLeave={() => setShowText(null)}
+                            onClick={() => setDragNDrop(artwork.id)}
+                            style={{
+                              height: "150px",
+                              width: "150px",
+                              overflow: "hidden",
+                              display: "flex",
+                              position: "relative",
+                            }}
+                          >
+                            <TextScreen
+                              $displayText={
+                                showText == artwork.id ? "initial" : "none"
+                              }
+                            >
+                              <p style={{ marginTop: "auto" }}>
+                                {artwork.artistName}
+                                <br />
+                                <i>
+                                  <strong>{artwork.title},</strong>
+                                </i>{" "}
+                                {artwork.year}
+                              </p>
+                            </TextScreen>
+                            <div>
+                              <img
+                                style={{
+                                  height: "auto",
+                                  minHeight: "150px",
+                                  minWidth: "150px",
+                                  zIndex: "-1",
+                                }}
+                                src={artwork.image}
+                              />
+                            </div>
+                          </div>
+                        </div>
+                      )}
+                    </Draggable>
+                  );
+                })}
+              {!showFavoriteArtworks &&
+                favoritePosts?.map((post, index) => {
+                  // const Template = templatesArr[page.type];
+                  return (
+                    <Draggable
+                      draggableId={post?.id}
+                      index={index}
+                      key={post?.id}
+                    >
+                      {(
+                        provided,
+                        snapshot ///change var names
+                      ) => (
+                        <div
+                          ref={provided.innerRef}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                          style={getItemStyle(
+                            snapshot.isDragging,
+                            provided.draggableProps.style
+                          )}
+                        >
+                          {/* independent div */}
+                          <PostWrapper key={post?.id}>
+                            <div>
+                              <h1>
+                                <strong>{post?.title}</strong>
+                              </h1>
+                              <p>{post?.postTime}</p>
+                            </div>
+                            <div>
+                              <p>
+                                <strong>Posted by: </strong>
+                                {post?.postBy}
+                              </p>
+                              <p>
+                                <strong>Artist: </strong>
+                                {post?.artist}
+                              </p>
+                            </div>
+                          </PostWrapper>
+                        </div>
+                      )}
+                    </Draggable>
+                  );
+                })}
+              {droppableProvided.placeholder}
+              {/* placeholder here */}
+            </div>
+          )}
+        </Droppable>
+      </div>
+      <DragInstruction>
+        <DragButton />
+        <div style={{ textAlign: "left" }}>
+          <strong>1. Scroll to see collections</strong>
+          <br />
+          <strong>2. Drag to adjust order</strong>
+        </div>
+      </DragInstruction>
+    </CollectionWrapper>
+  );
+}
--- pages/user-profile.tsx
+++ pages/user-profile.tsx
@@ -1,7 +1,5 @@
 import styled from "@emotion/styled";
-import Link from "next/link";
-import { useState, useContext, useEffect } from "react";
-import { ThemeColorContext } from "../context/ColorContext";
+import { useState, useEffect } from "react";
 import {
   collection,
   query,
@@ -11,34 +9,94 @@ import {
 } from "firebase/firestore";
 import { db } from "../config/firebase";
 import { useAuth } from "../context/AuthContext";
-import quotes from "../public/visitorJourney.json";
-import SignpostButton from "../components/Button";
+import visitorJourney from "../public/artist-info/visitorJourney.json";
+import { useRouter } from "next/router";
+import dynamic from "next/dynamic";
+import { DragDropContext } from "react-beautiful-dnd";
+// const CollectionColumn = dynamic(() => import("../components/DragNDrop"), {
+//   ssr: false,
+// });
 
-const FinalWords = styled.section`
-  width: 90vw;
-  margin: 0 auto;
-  text-align: left;
-  display: flex;
-  flex-direction: row;
-`;
-
+const CollectionColumn = dynamic(() => import("../components/DragNDrop"), {
+  ssr: false,
+});
 const Opening = styled.h1`
   font-size: 1.25rem;
+  text-align: left;
 `;
 
-const ColorStripe = styled.div<{ $colorCode: string }>`
-  background: ${(props) => props.$colorCode};
-  width: 18vw;
-  height: 100vh;
-  position: absolute;
-  top: 0;
-  left: 12vw;
+const TextWrapper = styled.div`
+  width: 500px;
+  padding: 0 50px;
+  margin: 20px auto 20px 0;
+  @media screen and (max-width: 950px) {
+    width: 80vw;
+    margin: 20px auto;
+    padding: 0;
+  }
+`;
+
+const ArtworkWrapper = styled.div`
+  display: flex;
+  width: 100vw;
+  min-height: 40vh;
+  margin: 0 auto;
+  justify-content: flex-start;
+  border-bottom: 1px solid black;
+  border-top: 1px solid black;
+  @media screen and (max-width: 950px) {
+    flex-direction: column;
+  }
+`;
+
+const ArtworkImage = styled.div`
+  width: 50vw;
+  background-size: cover;
+  background-position: center;
+  border-right: 1px solid black;
+  @media screen and (max-width: 950px) {
+    height: 50vh;
+    width: 100vw;
+  }
+  @media screen and (max-width: 600px) {
+    height: 40vh;
+  }
+  @media screen and (max-width: 600px) {
+    height: 30vh;
+  }
+`;
+
+const ButtonGroup = styled.div`
+  display: flex;
+  width: 80vw;
+  justify-content: space-between;
+  margin: 20px 0 20px 5vw;
+  column-gap: 20px;
+  @media screen and (max-width: 600px) {
+    flex-direction: column;
+    width: 90vw;
+  }
+`;
+const HalfButton = styled.button<{ $textColor: string; $bgColor: string }>`
+  height: fit-content;
+  font-size: 1.25rem;
+  padding: 10px 20px;
+  width: fit-content;
+  border: 1px solid black;
+  margin: 4px 0;
+  cursor: pointer;
+  color: ${(props) => props.$textColor};
+  background-color: ${(props) => props.$bgColor};
+  &:hover {
+    color: white;
+    background-color: #2c2b2c;
+  }
 `;
 
 interface IUser {
   email: string | undefined;
-  favoriteArtworksID: string[] | undefined;
-  favoritePostsID: string[] | undefined;
+  favoriteArtworksId: string[] | undefined;
+  favoritePostsId: string[] | undefined;
   id: string | undefined;
   last_changed: Timestamp | undefined;
   name: string | undefined;
@@ -53,125 +111,193 @@ interface EnumJourneyItem {
 
 interface IArtwork {
   artistName: string | undefined;
-  completionYear: number | undefined;
+  year: number | undefined;
   title: string | undefined;
   id: string | undefined;
   image: string | undefined;
 }
-
+interface IArtworks extends Array<IArtwork> {}
 interface EnumJourneyItems extends Array<EnumJourneyItem> {}
 
 export default function UserProfile() {
-  const [themeColor] = useContext(ThemeColorContext);
+  const [favoritePosts, setFavoritePosts] = useState([]);
+  const [showText, setShowText] = useState(null);
+  const [dragNDrop, setDragNDrop] = useState(null);
+  const router = useRouter();
+  const [showFavoriteArtworks, setShowFavoriteArtworks] = useState(true);
   const { user } = useAuth();
+
   const [profile, setProfile] = useState<IUser>({
     email: undefined,
-    favoriteArtworksID: undefined,
-    favoritePostsID: undefined,
+    favoriteArtworksId: undefined,
+    favoritePostsId: undefined,
     id: undefined,
     last_changed: undefined,
     name: undefined,
     visitorJourney: undefined,
   });
-  const [artwork, setArtwork] = useState<IArtwork>({
-    artistName: undefined,
-    completionYear: undefined,
-    title: undefined,
-    id: undefined,
-    image: undefined,
-  });
-  const [quote, setQuote] = useState("");
-  console.log(artwork);
+
+  const [artwork, setArtwork] = useState<IArtworks>([]);
+  const [quote, setQuote] = useState(
+    visitorJourney[0].quotes[
+      Math.floor(Math.random() * visitorJourney[0].quotes?.length)
+    ]
+  );
 
   useEffect(() => {
+    const exitingFunction = () => {
+      console.log(favoritePosts);
+      console.log(artwork);
+      console.log("exiting...");
+    };
+    router.events.on("routeChangeStart", exitingFunction);
+
     const getProfile = async () => {
       const q = query(collection(db, "users"), where("id", "==", user?.uid));
       const querySnapshot = await getDocs(q);
       querySnapshot.forEach((doc) => {
         const IUser = doc.data() as IUser;
         setProfile(IUser);
-        const artworkID =
-          IUser.favoriteArtworksID[IUser.favoriteArtworksID.length - 1];
-        getFavoriteArtwork(artworkID);
+        const artworkID = IUser.favoriteArtworksId;
+        setFavoritePosts(IUser?.favoritePostsId);
+        if (artworkID) {
+          getFavoriteArtwork(artworkID);
+        }
       });
     };
     const getFavoriteArtwork = async (id) => {
-      const q = query(collection(db, "artists"), where("id", "==", id));
-      const querySnapshot = await getDocs(q);
-      const docs = querySnapshot.docs.map((doc) => doc.data() as any);
-      setArtwork(docs[0]);
-      const quoteNumber = quotes.filter(
-        (q) => q.artistName == docs[0]?.artistName
-      )[0].quotes.length;
-      setQuote(
-        quotes.filter((q) => q.artistName == docs[0]?.artistName)[0].quotes[
-          Math.round(Math.random() * quoteNumber)
-        ]
-      );
+      setArtwork(id);
+      const artistQuotes = visitorJourney?.filter(
+        (j) => j?.artistName == id[id?.length - 1]?.artistName
+      )[0]?.quotes;
+      if (artistQuotes) {
+        setQuote(
+          artistQuotes[Math.floor(Math.random() * artistQuotes?.length)]
+        );
+      }
     };
 
     getProfile();
-  }, []);
+
+    return () => {
+      console.log("unmounting component...");
+      router.events.off("routeChangeStart", exitingFunction);
+    };
+  }, [user?.uid]);
+
+  const reorderArtworks = (destination, source) => {
+    const newArtworkOrder = [...artwork];
+    const [remove] = newArtworkOrder.splice(source.index, 1);
+    newArtworkOrder.splice(destination.index, 0, remove);
+    setArtwork(newArtworkOrder);
+  };
+
+  const reorderPosts = (destination, source) => {
+    const newPostOrder = [...favoritePosts];
+    const [remove] = newPostOrder.splice(source.index, 1);
+    newPostOrder.splice(destination.index, 0, remove);
+    setFavoritePosts(newPostOrder);
+  };
+
+  // const [remove] = newPagesOrder.splice(source.index, 1);
+  console.log(artwork.map((art) => art.id).includes(dragNDrop));
+  const onDragEnd = (result) => {
+    const { destination, source } = result;
+    if (!destination) return;
+
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+    if (showFavoriteArtworks) {
+      reorderArtworks(destination, source);
+      return;
+    }
+    reorderPosts(destination, source);
+  };
   return (
-    <div style={{ paddingTop: "104px" }}>
-      <FinalWords>
-        <div style={{ width: "10vw" }}></div>
-        <div
-          style={{
-            width: "200px",
-            position: "absolute",
-            top: "33vh",
-            left: "19vw",
-            zIndex: "3",
-          }}
-        >
-          <p style={{ fontSize: "1.25rem" }}>
-            <strong>
-              {`"${quote}"`}
-              <br />-{artwork.artistName}
-            </strong>
-          </p>
-        </div>
-        <ColorStripe
-          $colorCode={themeColor ? `${themeColor.secondary}` : "#BBB6AC"}
-        ></ColorStripe>
-        <div
-          style={{
-            backgroundImage: `url(${artwork.image})`,
-            minWidth: "500px",
-            height: "60vh",
-            backgroundSize: "cover",
-            marginLeft: "310px",
-          }}
+    <DragDropContext onDragEnd={(e) => onDragEnd(e)}>
+      {/* add event listener */}
+      <div style={{ paddingTop: "15px" }}>
+        <ArtworkWrapper>
+          <ArtworkImage
+            style={{
+              backgroundImage: `url(${artwork[artwork.length - 1]?.image}`,
+            }}
+          />
+          <TextWrapper>
+            <Opening>
+              <strong>
+                Thank you, <span>{profile?.name},</span> for your visit to the
+                Healing Museum.
+              </strong>
+            </Opening>
+            <br />
+            {artwork.length == 0 && (
+              <p>
+                You haven&apos;t collected any artworks yet, start exploring!
+              </p>
+            )}
+            {artwork.length > 0 && (
+              <>
+                <p>
+                  <strong>
+                    <i>{artwork[artwork.length - 1]?.title}</i>
+                  </strong>{" "}
+                  by {artwork[artwork.length - 1]?.artistName} is the last
+                  painting you saved as favorite during your journey. <br />
+                  We hope this painting gives you strength, for knowing there is
+                  someone who shares your feelings.
+                </p>
+                <br />
+                <p>
+                  <strong>
+                    {`"${quote}"`}
+                    <br />-{artwork[artwork.length - 1]?.artistName}
+                  </strong>
+                </p>
+              </>
+            )}
+          </TextWrapper>
+        </ArtworkWrapper>
+        <ButtonGroup>
+          <div>
+            <HalfButton
+              onClick={() => setShowFavoriteArtworks(true)}
+              $textColor={showFavoriteArtworks ? "white" : "black"}
+              $bgColor={showFavoriteArtworks ? "#2c2b2c" : "transparent"}
+            >
+              Favorite Artworks
+            </HalfButton>
+            <HalfButton
+              onClick={() => setShowFavoriteArtworks(false)}
+              $textColor={showFavoriteArtworks ? "black" : "white"}
+              $bgColor={showFavoriteArtworks ? "transparent" : "#2c2b2c"}
+            >
+              Favorite Posts
+            </HalfButton>
+          </div>
+
+          <HalfButton
+            $textColor="white"
+            $bgColor="#2c2b2c"
+            onClick={() => router.push("/theme-color")}
+          >
+            Start a new journey
+          </HalfButton>
+        </ButtonGroup>
+        <CollectionColumn
+          showFavoriteArtworks={showFavoriteArtworks}
+          artwork={artwork}
+          setShowText={setShowText}
+          showText={showText}
+          favoritePosts={favoritePosts}
+          dragNDrop={dragNDrop}
+          setDragNDrop={setDragNDrop}
         />
-
-        <div style={{ width: "350px", padding: "0 40px" }}>
-          <Opening>
-            <strong>
-              Thank you, {profile?.name}, <br />
-              for your visit to the Healing Museum.
-            </strong>
-          </Opening>
-          <p>
-            I hope you had a nice time and some understandings of the artist
-            whose life perspectives resonate with your own.
-          </p>
-          <p>
-            <strong>
-              <i>{artwork.title}</i>
-            </strong>{" "}
-            by {artwork.artistName} is the last painting you saved as favorite
-            during your journey, <br />
-            We hope this painting gives you strength, <br />
-            for knowing there is someone who shares your feelings.
-          </p>
-
-          <span>{artwork.completionYear}</span>
-        </div>
-      </FinalWords>
-      <SignpostButton href="/visitor-posts">
-        Read about how other visitors feel
-      </SignpostButton>
-    </div>
+      </div>
+    </DragDropContext>
   );
 }
